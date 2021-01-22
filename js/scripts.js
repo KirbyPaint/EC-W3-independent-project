1d6e87aff07592ac75ca96f60ac17878e0435019
Ashley Porter <porter.ashley13@gmail.com>
2021-01-22 13:15:27 -0800

    add optional name insert textbox

--- js/scripts.js
+++ js/scripts.js
@@ -57,6 +57,26 @@ function reverse(input) {
   return formatArray;
 }
 
+function arrayNameTest(input, name) {
+  const outputArray = [];
+  for (let i = 0; i <= input; i++) {
+    if ([i].toString().includes("3")) {
+      outputArray.push("Won't you be my neighbor, " + name + "?");
+    }
+    else if ([i].toString().includes("2")) {
+      outputArray.push("Boop");
+    }
+    else if ([i].toString().includes("1")) {
+      outputArray.push("Beep");
+    }
+    else {
+      outputArray.push(i);
+    }
+  }
+  const formatArray = outputArray.join(", ");
+  return formatArray;
+}
+
 $(document).ready(function() {
   $("button#lineByLine").click(function() {
     const input = $("input#inputText").val();
@@ -78,7 +98,16 @@ $(document).ready(function() {
   $("#inputForm").submit(function(event) {
     event.preventDefault();
     const input = $("input#inputText").val();
-    const outputArray = arrayTest(input);
+    const name = $("input#inputName").val();
+    constOutputArray = [];
+    if (name) {
+      outputArray = arrayNameTest(input, name);
+    }
+    else {
+      outputArray = arrayTest(input);
+    }
+
+    // const outputArray = arrayTest(input);
     $(".return").empty();
     $(".return").append(outputArray);
   });
